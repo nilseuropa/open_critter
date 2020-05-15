83ec6098e7d513c9cf8ec6c8940c48d5112fc86f
Marton Yuhaas <marton@nowtech.hu>
2020-05-15 18:29:46 +0200

    transparent bridge

--- src/robot_bridge.cpp
+++ src/robot_bridge.cpp
@@ -1,6 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <open_critter/protocol.h>
+
+int sockfd, portno, serverlen;
+struct sockaddr_in serveraddr;
+struct hostent *server;
 
 double map(double x, double in_min, double in_max, double out_min, double out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -8,6 +20,14 @@ double map(double x, double in_min, double in_max, double out_min, double out_ma
 
 void callback(const std_msgs::Float64::ConstPtr &msg, const int &jointId) {
     ROS_INFO_STREAM("Joint id: " << jointId << "\t Norm:" << msg->data);
+    ServoStatePacket packet;
+    packet.header.packetId = PID_SET_SERVO;
+    packet.jointId = jointId;
+    packet.state   = msg->data;
+    uint8_t buf[sizeof(ServoStatePacket)];
+    memcpy( buf, &packet, sizeof(ServoStatePacket) );
+    int ret = sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    if (ret < 0) ROS_ERROR("Can not send.");
 }
 
 int main(int argc, char** argv){
@@ -15,11 +35,28 @@ int main(int argc, char** argv){
   ros::NodeHandle n;
   ros::NodeHandle nhLocal("~");
 
-  double publish_rate;
-  std::string ns;
   std::string cs = "/command";
+  std::string ns;
+  std::string hostname;
 
   nhLocal.param("namespace", ns, std::string("open_critter"));
+  nhLocal.param("hostname", hostname, std::string("zero"));
+  nhLocal.param("portnumber", portno, 9001);
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) ROS_ERROR("ERROR opening socket");
+
+  server = gethostbyname(hostname.c_str());
+  if (server == NULL) {
+      ROS_ERROR("ERROR, no such host as %s\n", hostname.c_str());
+      exit(0);
+  }
+
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr,
+  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+  serveraddr.sin_port = htons(portno);
 
   // Coxae
   ros::Subscriber FR_COX_sub = n.subscribe<std_msgs::Float64>(ns+"/"+"front_right_coxa"+cs, 1, boost::bind(callback, _1, FR_Coxa));
@@ -40,5 +77,10 @@ int main(int argc, char** argv){
   ros::Subscriber RL_TIB_sub = n.subscribe<std_msgs::Float64>(ns+"/"+"rear_left_tibia"+cs,   1, boost::bind(callback, _1, RL_Tibia));
 
   ros::spin();
+  // while(n.ok()){
+  //   ros::spinOnce();
+  //   int ret = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&serveraddr, &serverlen);
+  //   if (ret < 0) ROS_ERROR("Can not receive.");
+  // }
   return 0;
 }
